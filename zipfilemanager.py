71185b8a3f16e7a9f48f8b1f563898df8bf4cac6
Lima_oncode <lucas-lima1974@hotmail.com>
2024-11-22 12:20:45 -0300

    Update: Extração de arquivos .zip e .7z com a exclusão do mesmo após extração

--- zipfilemanager.py
+++ zipfilemanager.py
@@ -1,4 +1,5 @@
 import zipfile
+import py7zr
 import os
 
 class ZipFileManager:
@@ -8,16 +9,37 @@ class ZipFileManager:
         
     def extract_zip_files(self):
         if os.path.isdir(self.sys_path):
-            list_sys = os.listdir(self.sys_path)
+            list_sys = [file for file in os.listdir(self.sys_path) if file.endswith(".zip") or file.endswith(".7z")]
             print(f"Arquivos encontrados no diretório de origem: {list_sys}")
             
             for file in list_sys:
-                if file.endswith(".zip"):
-                    zip_file_path = os.path.join(self.sys_path, file)
-                    
-                    with zipfile.ZipFile(file=zip_file_path, mode="r") as zip_obj:
-                        zip_obj.extractall(self.end_path)
-                        print(f"Extraiu o arquivo {file} no diretório {self.end_path}")
+                zip_file_path = os.path.join(self.sys_path, file)
+                zip_file_path = os.path.normpath(zip_file_path)
+
+                print(zip_file_path)
+
+                folder_name = os.path.splitext(file)[0]
+                extract_path = os.path.join(self.end_path, folder_name)
+                os.makedirs(extract_path, exist_ok=True)
+
+                try:
+                    if zip_file_path.endswith(".zip"):
+                        with zipfile.ZipFile(file=zip_file_path, mode="r") as zip_obj:
+                            zip_obj.extractall(extract_path)
+                            print(f"Arquivo ZIP '{file}' extraído para {self.end_path}")
+                    elif zip_file_path.endswith(".7z"):
+                        with py7zr.SevenZipFile(file=zip_file_path, mode="r") as sevenz_obj:
+                            sevenz_obj.extractall(extract_path)
+                            print(f"Arquivo 7z '{file}' extraído para {extract_path}")
+                except zipfile.BadZipFile:
+                    print(f"O arquivo '{file}' não é um ZIP válido.")
+                except py7zr.Bad7zFile:
+                    print(f"O arquivo '{file}' não é um 7z válido.")
+                except Exception as e:
+                    print(f"Erro inesperado ao processar '{file}': {e}")
+                
+                os.remove(zip_file_path)
+
     
     def rename_files(self):
         list_extract_dir = os.listdir(self.end_path)
@@ -31,9 +53,11 @@ class ZipFileManager:
                 print(f"Renomeou o arquivo {file} para {new_path}")
 
 # Exemplo de uso da classe ZipFileManager -> Instanciando metodos
-sys_path = "F:/mycodes/zipfilemanager/zips"
-end_path = "F:/mycodes/zipfilemanager/extracts"
+sys_path = "D:/JOGOS/PCSX2/PS2 ISO"
+end_path = "D:/JOGOS/PCSX2/PS2 ISO"
+
+
 
 zip_manager = ZipFileManager(sys_path, end_path)
 zip_manager.extract_zip_files()
-zip_manager.rename_files()
+# zip_manager.rename_files()
